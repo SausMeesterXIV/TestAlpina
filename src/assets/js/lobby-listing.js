2b8869598a3e0bf8ae3c76957f15161e97ec7a47
Pieter Vermeulen <Pieter.VERMEULEN@student.howest.be>
2026-03-25 13:25:06 +0100

    Splits renderGameList() into multiple functions

--- src/assets/js/lobby-listing.js
+++ src/assets/js/lobby-listing.js
@@ -4,19 +4,31 @@ function init () {
     renderGameList();
 }
 
-function renderGameList() {
-    const $template = document.querySelector("#expedition-template");
-    fetchFromServer("/games?started=false")
-        .then(resp => {
-            resp.games.forEach(game => {
-                const $clone = document.importNode($template.content, true);
-                $clone.querySelector(".comment").textContent = `ID: ${game.gameId}`;
-                $clone.querySelector("h2").textContent = game.gameName;
-                $clone.querySelector("h2+p").textContent = `${game.players.length}/${game.numberOfPlayers} players`;
+function fetchGames() {
+    return fetchFromServer("/games?started=false")
+        .then(resp => resp.games);
+}
 
-                document.querySelector("#expeditions").appendChild($clone);
-            })
-        });
+function createGameElement(game) {
+    const $template = document.querySelector("#expedition-template");
+    const $clone = document.importNode($template.content, true);
+
+    $clone.querySelector(".comment").textContent = `ID: ${game.gameId}`;
+    $clone.querySelector("h2").textContent = game.gameName;
+    $clone.querySelector("h2+p").textContent = `${game.players.length}/${game.numberOfPlayers} players`;
+
+    return $clone;
+}
+
+function renderGames(games) {
+    const $target = document.querySelector("#expeditions");
+    games.forEach(game => {
+        $target.appendChild(createGameElement(game));
+    })
+}
+
+function renderGameList() {
+    fetchGames().then(renderGames)
 }
 
 init();
