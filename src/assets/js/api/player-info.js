6ffc12578188245d3fdae812e0230c6a1c5f9841
olivier <olivier.duquet@student.howest.be>
2026-03-26 15:57:17 +0100

    all players are gettings savedi n local storage

--- src/assets/js/api/player-info.js
+++ src/assets/js/api/player-info.js
@@ -1,10 +1,22 @@
-import {fetchSpecificGame} from "./game-info.js";
+  import {fetchSpecificGame} from "./game-info.js";
 
-function fetchPlayerInfo(gameId) {
-  return fetchSpecificGame(gameId)
-    .then(game => {
-        return game.players;
-    });
-}
+  function fetchPlayerInfo(gameId) {
+    return fetchSpecificGame(gameId)
+      .then(game => {
+          return game.players;
+      });
+  }
 
-export {fetchPlayerInfo};
+  function savePlayersToLocalStorage(gameId) {
+    fetchPlayerInfo(gameId)
+      .then(players => {
+        localStorage.setItem(`players_${gameId}`, JSON.stringify(players));
+      })
+      .catch(error => {
+        console.error("Error fetching player info:", error);
+      }
+    );
+  }
+
+
+  export {fetchPlayerInfo, savePlayersToLocalStorage};
