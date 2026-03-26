fe39a38e8817c094fea16f9e4d4697b6b8028198
Rhune Betina <rhune.BETINA@student.howest.be>
2026-03-26 14:35:46 +0100

    base of joining game.

---
+++ src/assets/js/api/join-game.js
@@ -0,0 +1,18 @@
+import {defaultPlayerName, defaultPlayerColor} from "../calculate-default-player-values.js";
+import {fetchFromServer} from "../data-connector/api-communication-abstractor.js";
+
+function joinGame(gameId){
+  defaultPlayerName(gameId)
+    .then(name =>{
+      return name;
+    })
+    .then(name => defaultPlayerColor(gameId)
+      .then(color => {
+        const url = `/games/${gameId}/hikers/${color}`
+        return {name, url};
+    }))
+    .then(data =>{
+      const url = data.url;
+      return fetchFromServer(url,"POST", { playerName: data.name })
+    });
+}
