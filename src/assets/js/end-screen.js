0a412c86864019c0c507e5dff5b4fb7ee5c1f59a
Pieter Vermeulen <Pieter.VERMEULEN@student.howest.be>
2026-03-26 16:03:47 +0100

    Changes the background depending on the acquired score

--- src/assets/js/end-screen.js
+++ src/assets/js/end-screen.js
@@ -1,19 +1,35 @@
+import { fetchGameDetails } from "./api/game-info.js";
 import { loadFromStorage } from "./data-connector/local-storage-abstractor.js"
 
 function init() {
     setBackground();
 }
 
-function getPlayerToken() {
-    return loadFromStorage("playerToken");
+function getPlayerName() {
+    return loadFromStorage("playerName");
 }
 
 function getGameId() {
-    return loadFromStorage("gameId");
+    return Number(loadFromStorage("gameId")); //Makes sure the gameId is returned as an int and not a String.
+}
+
+function calcBestPlayer(players) {
+    return players.reduce((currentBest, player) => { // iterates over the array until only the player with the best score remains
+        if (player.score > currentBest.score) {
+            return player;
+        }
+        return currentBest;
+    })
+}
+
+function hasMostPoints(players) {
+    return getPlayerName() === calcBestPlayer(players).name;
 }
 
 function setBackground() {
-
+    const $body = document.querySelector("body");
+    fetchGameDetails(getGameId())
+        .then(data => hasMostPoints(data.players) ? $body.classList.add("victory") : $body.classList.add("defeat")); // typical if-else structure, but simplified notation
 }
 
 init();
