696be03e5ab90b883c254bb3ed9e28dcc7453b19
Pieter Vermeulen <Pieter.VERMEULEN@student.howest.be>
2026-03-26 15:12:02 +0100

    Adds getGameId()

--- src/assets/js/end-screen.js
+++ src/assets/js/end-screen.js
@@ -1,11 +1,19 @@
 import { loadFromStorage } from "./data-connector/local-storage-abstractor.js"
 
 function init() {
-
+    setBackground();
 }
 
 function getPlayerToken() {
     return loadFromStorage("playerToken");
+}
+
+function getGameId() {
+    return loadFromStorage("gameId");
+}
+
+function setBackground() {
+
 }
 
 init();
