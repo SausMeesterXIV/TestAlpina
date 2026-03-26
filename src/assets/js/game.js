06271554a2f437c7d0670735cf2eeb661def445b
brent.monkerhey <brent.monkerhey@student.howest.be>
2026-03-26 11:45:44 +0100

    fix dataset card-id and added points to cards

--- src/assets/js/game.js
+++ src/assets/js/game.js
@@ -3,16 +3,15 @@ import {fetchFromServer} from "./data-connector/api-communication-abstractor.js"
 
 const arrayOfCards =
   [{id: 50, animal: "chamois", landscape: "mountain", victoryPointCondition: {basescore: 0, score: 2, selector: "DW", filter: "HI"}},
-  {id: 50, animal: "chamois", landscape: "mountain", victoryPointCondition: {basescore: 0, score: 2, selector: "DW", filter: "HI"}},
-  {id: 50, animal: "chamois", landscape: "mountain", victoryPointCondition: {basescore: 0, score: 2, selector: "DW", filter: "HI"}},
-  {id: 50, animal: "chamois", landscape: "mountain", victoryPointCondition: {basescore: 0, score: 2, selector: "DW", filter: "HI"}},
-  {id: 50, animal: "chamois", landscape: "mountain", victoryPointCondition: {basescore: 0, score: 2, selector: "DW", filter: "HI"}},
-  {id: 50, animal: "chamois", landscape: "mountain", victoryPointCondition: {basescore: 0, score: 2, selector: "DW", filter: "HI"}}]
+  {id: 50, animal: "frog", landscape: "mountain", victoryPointCondition: {basescore: 0, score: 2, selector: "DW", filter: "HI"}},
+  {id: 50, animal: "chamois", landscape: "forest", victoryPointCondition: {basescore: 0, score: 2, selector: "DW", filter: "HI"}},
+  {id: 50, animal: "chamois", landscape: "lake", victoryPointCondition: {basescore: 0, score: 2, selector: "DW", filter: "HI"}},
+  {id: 50, animal: "nutcracker", landscape: "mountain", victoryPointCondition: {basescore: 0, score: 2, selector: "DW", filter: "HI"}},
+  {id: 50, animal: "frog", landscape: "mountain", victoryPointCondition: {basescore: 0, score: 2, selector: "DW", filter: "HI"}}]
 //for testing purposes
 
 
 function init() {
-  fetchAllCards();
   renderHand(arrayOfCards);
 }
 
@@ -23,14 +22,13 @@ function renderHand(cardArray) {
 
   cardArray.forEach(card => {
     const $clone = $template.content.cloneNode(true);
-    $clone.querySelector("article").id = card.id;
+    $clone.querySelector("article").dataset.cardId = card.id;
     $clone.querySelector("img").src = `images/${card.animal}_${card.landscape}.png`;
-    $clone.querySelector("p").textContent = `${card.victoryPointCondition.selector} - ${card.victoryPointCondition.filter}`;
+    $clone.querySelector("p").textContent = `${card.victoryPointCondition.basescore} + ${card.victoryPointCondition.score} / ${card.victoryPointCondition.selector} --- ${card.victoryPointCondition.filter}`;
     $fragment.appendChild($clone);
   })
 
   document.querySelector("#hand").appendChild($fragment);
 }
-
 
 init();
