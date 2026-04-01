073f2f27925741fa5881a9255df2810008d385d5
Rhune Betina <rhune.BETINA@student.howest.be>
2026-04-01 15:47:31 +0200

    adding: fetch game board for easy access to board

--- src/assets/js/api/game-info.js
+++ src/assets/js/api/game-info.js
@@ -27,8 +27,12 @@ function fetchGameDetails(gameId) {
   return fetchFromServer(`/games/${gameId}`);
 }
 
+function fetchGameBoard(gameId){
+  return fetchFromServer(`/games/${gameId}/board`);
+}
 export{
   fetchSpecificGame,
   fetchUnstartedGames,
-  fetchGameDetails
+  fetchGameDetails,
+  fetchGameBoard
 };
