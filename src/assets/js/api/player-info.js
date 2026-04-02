a2f283235b87f5db41ffa4866408fdfff1e740cb
brent.monkerhey <brent.monkerhey@student.howest.be>
2026-04-02 16:17:11 +0200

    more merge issues

--- src/assets/js/api/player-info.js
+++ src/assets/js/api/player-info.js
@@ -3,7 +3,7 @@ import {fetchFromServer} from "../data-connector/api-communication-abstractor.js
 import {loadFromStorage} from "../data-connector/local-storage-abstractor.js";
 import {getGameId, getHiker} from "../storage-utils.js";
 
-<<<<<<< HEAD
+
 function fetchPlayerInfo(gameId) {
   return fetchSpecificGame(gameId)
     .then(game => {
@@ -16,13 +16,4 @@ function fetchPlayerHand() {
 }
 
 export {fetchPlayerInfo, fetchPlayerHand};
-=======
-  function fetchPlayerInfo(gameId) {
-    return fetchSpecificGame(gameId)
-      .then(game => {
-          return game.players;
-      });
-  }
 
-  export {fetchPlayerInfo};
->>>>>>> 9af7eb70d4b79be57bf9c018cfbe5eeb84d07ea4
