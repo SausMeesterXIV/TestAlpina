e980d828188dcd8d9beacfdd14fb5343be52d0e0
dayvid <dayvid.van.marcke@student.howest.be>
2026-04-12 19:14:22 +0200

    file management of the game-logic inside game.js

--- src/assets/js/game.js
+++ src/assets/js/game.js
@@ -7,11 +7,14 @@ import {renderBoard} from "./renderers/gameboard-renderer.js";
 import {renderHand} from "./renderers/hand-renderer.js";
 import {remainingHikers} from "./renderers/hiker-renderer.js";
 
-//board logic
+//logic
 import {handleTileClick, endTurnButton} from "./logic/board-logic.js";
+import * as gameLogic from "./logic/game-logic.js";
 
-let selectedCard = null;
-// object that will store the moves done by the player.
+//vars
+import {selectedCard} from "./logic/game-logic.js";
+
+
 let hasPlacedHiker = false;
 let lastBoardState = null; // makes sure the browser knows whether the board the player sees is the same as the one saved in the server
 let currentPlayer = null //new function to do this.
@@ -23,14 +26,14 @@ function init() {
 
   addEventListeners();
 
-  setProgressBar(); // sets the initial and max values of the progress bar
+  gameLogic.setTimeProgressBar(); // sets the initial and max values of the progress bar
   tick(); // updates the progress bar every second
 
   gameLoop();
   renderLoop();
 
   remainingHikers();
-  placeHikerOnCard();
+  gameLogic.placeHikerOnCard();
 }
 
 function addEventListeners() {
@@ -40,10 +43,10 @@ function addEventListeners() {
 
   // for selecting a card
   const $hand = document.querySelector("#hand");
-  $hand.addEventListener('click', selectCard, true);
+  $hand.addEventListener('click', gameLogic.selectCard, true);
 
   //hiker
-  document.querySelector("#select-hiker-button").addEventListener("click", selectHiker);
+  document.querySelector("#select-hiker-button").addEventListener("click", gameLogic.selectHiker);
 
   //endTurnButton
   document.querySelector("#end-turn-button").addEventListener("click", endTurn);
@@ -67,19 +70,6 @@ function renderLoop() {
   });
 }
 
-function selectCard(e){
-  const $clickedCard = e.target.closest('article.card');
-
-  const allCardsInHand = document.querySelectorAll('#hand .card');
-  allCardsInHand.forEach(card => {
-      card.classList.remove('selected');
-  });
-
-  $clickedCard.classList.add('selected');
-
-  selectedCard = $clickedCard;
-}
-
 function updateCurrentPlayer(data) {
   const currentHiker = data.currentHiker;  // gets the current hiker color.
   const currentPlayer = data.players.find(player => player.hiker === currentHiker); // finds the hiker which has the same color as current hiker
@@ -87,55 +77,9 @@ function updateCurrentPlayer(data) {
   if (currentPlayer) document.querySelector("#turn-name").textContent = `${currentPlayer.name}'s turn`; // the if statement is to make sure it only changes if .find() found a result
 }
 
-function updateProgressBar() {
-  document.querySelector("progress").value += 1;
-  if (document.querySelector("progress").value >= document.querySelector("progress").max) {
-    document.querySelector("progress").value = 0;
-    /*endturn()*/ //currently there is no end turn function
-  }
-}
-
 function tick() {
-  setInterval(updateProgressBar, 1000);
-}
-
-function setProgressBar() {
-  /*document.querySelector("progress").max = loadFromStorage(timePerTurn)*/ //there is currently no timeperturn in localstorage
-  document.querySelector("progress").max = 60; //temporary
-}
-
-function selectHiker(){
-  document.querySelector("main").classList.toggle("hiker-image");
-}
-
-function placeHikerOnCard() {
-  let canPlayHiker = true;
-  const gameId = Number(storageHandler.getGameId())
-  fetchGameDetails(gameId)
-    .then(data => data.players)
-    .then(players => {
-      players.forEach(player => {
-        if (player.hikersLeft <= 0) {
-          canPlayHiker = false;
-        }
-      })
-      if (canPlayHiker){
-        const cards = document.querySelectorAll(".card"); // class/selector needs to be changed so only the cards in a grid are selected
-        const hiker = document.querySelector(".hiker");
-        const main = document.querySelector("main");
-
-        //following code up for change
-        cards.forEach(card => {
-          card.addEventListener("click", () => {
-            //this code moves the hiker
-            if (main.classList.contains("hiker-image")) {
-              card.appendChild(hiker);
-              hiker.classList.remove("hidden");
-            }
-          });
-        });
-      }
-    })
+  setInterval(gameLogic.updateProgressBar, 1000);
+  //more can be added
 }
 
 function endTurn() {
@@ -149,7 +93,7 @@ function endTurn() {
   endTurnButton();
   renderHand();
   gameLoop();
-}
+} //game-logic up for discussion
 
 function gameLoop() {
   const $endTurnButton = document.querySelector("#end-turn-button");
